8140ac29f28877cadf45441613a2356951cc9d18
Aporlorxl23 <57567358+Aporlorxl23@users.noreply.github.com>
2020-06-09 00:42:49 +0300

    Update HackTheBox.py

--- HackTheBox.py
+++ HackTheBox.py
@@ -5,7 +5,6 @@ Created on Thu Apr 30 02:23:30 2020
 @Author: Aporlorxl23
 """
 import requests, json, base64
-from bs4 import BeautifulSoup
 
 Url = "https://www.hackthebox.eu/api/invite/generate"
 Agent = {"User-Agent":"Aporlorxl23 HackTheBox Invite Code Script"}
